334cc024a7b17dbd5a8a62662e4a26079ca6fd2e
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:24:26 -0300

    Add files via upload

---
+++ 06-ParImpar/06-ParImpar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06_ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int a;
+              Console.WriteLine("Ingrese un numero entero: ");
+              a = Convert.ToInt32(Console.ReadLine());
+              if (a % 2 == 0)
+              {
+                  Console.WriteLine("El numero es par.");
+              }
+              else
+              if (a % 2 != 0)
+              {
+                  Console.WriteLine("El numero es impar.");
+              }
+              Console.ReadKey();
+          }*/
+
+            for (int numero = 0; numero<=50; numero++)
+            {
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine(numero + " " + "es par.");
+                }
+                else if (numero %3== 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
